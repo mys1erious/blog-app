35789931adf980d5b81ae5d0d4d5e0f1d80db704
mys1erious <subtletyzeros@gmail.com>
2022-10-17 01:14:58 +0200

    moved from local to git

---
+++ env_loading.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def load_env_variable(key):
+    try:
+        return os.getenv(key)
+    except Exception:
+        raise OSError(f'{key} environment variable must be specified')
